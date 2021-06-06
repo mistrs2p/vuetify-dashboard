83cbea7a82df71e3d2d19dcd5906aaef0aeff86e
mistrs2p <39363439+mistrs2p@users.noreply.github.com>
2021-06-06 15:56:49 +0430

    Footer Added $ Bar Btn Configured

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,8 @@
     <v-app-bar app color="primary" dark>
       <v-toolbar-title>Vuetify Dashboard</v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn>Home</v-btn>
-      <v-btn>Login</v-btn>
+      <v-btn text rounded>Home</v-btn>
+      <v-btn text rounded>Login</v-btn>
     </v-app-bar>
     <v-content>
       <!-- Login Module -->
@@ -34,6 +34,32 @@
         </v-card-action>
       </v-card>    
     </v-content>
+    <v-footer
+      color="primary lighten-1"
+      padless
+    >
+      <v-row
+        justify="center"
+        no-gutters
+      >
+        <v-btn
+          v-for="link in links"
+          :key="link"
+          color="white"
+          text
+          rounded
+          class="my-2"
+        >
+          {{ link }}
+        </v-btn>
+        <v-col
+          class="primary lighten-2 py-4 text-center white--text"
+          cols="12"
+        >
+          {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
+        </v-col>
+      </v-row>
+    </v-footer>
   </v-app>
 </template>
 <script>
@@ -44,7 +70,15 @@ export default {
   },
 
   data: () => ({
-    showPass: false
+    showPass: false,
+    links: [
+      'Home',
+      'About Us',
+      'Team',
+      'Services',
+      'Blog',
+      'Contact Us',
+    ],
   }),
 };
 </script>

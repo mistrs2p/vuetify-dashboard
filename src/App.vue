c6139c3d07b9b8829c44f9a995b565935c7c1645
mistrs2p <39363439+mistrs2p@users.noreply.github.com>
2021-06-06 15:41:24 +0430

    Total Desigh Login Page Coded

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,29 @@
 <template>
   <v-app>
-    <v-card>
+    <v-card width="600" class="mx-auto mt-5">
       <v-card-title>
         <h1 class="display-1">Login</h1>  
       </v-card-title>
+      <v-card-text>
+        <v-form>
+          <v-text-field 
+            label="Username"
+            prepend-icon="mdi-account-circle"
+          ></v-text-field>
+          <v-text-field 
+            label="Password"
+            :type="showPass ? 'text' : 'password' "
+            prepend-icon="mdi-lock"
+            :append-icon="showPass ? 'mdi-eye' : 'mdi-eye-off'"
+            @click:append="showPass = !showPass"
+          ></v-text-field>
+        </v-form>
+      </v-card-text>
+      <v-divider></v-divider>
+      <v-card-action class="d-flex justify-space-between">
+        <v-btn color="success">Register</v-btn>
+        <v-btn color="info">Login</v-btn>
+      </v-card-action>
     </v-card>    
   </v-app>
 </template>
@@ -15,7 +35,7 @@ export default {
   },
 
   data: () => ({
-    //
+    showPass: false
   }),
 };
 </script>
